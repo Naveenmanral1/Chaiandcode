00c333d3da4e5a8446877958bffcc9cec3857cde
Naveenmanral1 <123979484+Naveenmanral1@users.noreply.github.com>
2024-03-17 21:46:36 +0530

    user testing on postman

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,14 +1,22 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
-import {ApiError} from "../utils/ApiError.js"
-import {user} from "../models/user.model.js"
-import {cloudinary} from "../utils/cloudinary.js"
+import {ApiError} from "../utils/ApiError.js";
+import {User} from "../models/user.model.js";
+import {uploadOnCloudinary} from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req , res) =>{
     //get user detail from frontend
     //validation - not empty
+    // check if user already exists : username , email
+    // check for image and avatar
+    // upload them to cloudinary , avatar
+    // create user object , create entry in db
+    // romove password and referesh token field from response
+    // check for user creation 
+    // return res
 
-    const{fullname,email,username,password} = req.body
+
+    const {fullname,email,username,password} = req.body
      
     if(
         [fullname,email,username,password].some((field)=>
@@ -17,7 +25,7 @@ const registerUser = asyncHandler(async (req , res) =>{
             throw new ApiError(400,"All fields are required")
         }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({    // user to User kiya h
         $or:[{ username },{ email }]
     })
 
@@ -25,8 +33,9 @@ const registerUser = asyncHandler(async (req , res) =>{
         throw new ApiError(409,"User with email or username already exist")
     }
 
-    const avatarLocalPath = req.files?.avatar[0]?.path;
-    const converImageLocalPath = req.files?.coverImage[0]?.path;
+    const avatarLocalPath = req.files?.avatar?.[0]?.path;
+    //?. after cover image help ki agr to cover image upload nhi bhi kreag to y error nhi dega
+    const converImageLocalPath = req.files?.coverImage?.[0]?.path;
 
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar file is required")
@@ -49,7 +58,7 @@ const registerUser = asyncHandler(async (req , res) =>{
     })
 
     // check the spelling or captial letter in user/User
-    const createdUser = await user.findById(user._id).select(
+    const createdUser = await User.findById(user._id).select(
         "-password -refreshToken"
     )
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js ";
-import { upload } from "../middlewares/multer.middlerware.js";
+import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 const router = Router()
 
 router.route ("/register").post(
@@ -16,4 +16,4 @@ router.route ("/register").post(
     ]),
     registerUser)
 
-export default router
+export default router  
